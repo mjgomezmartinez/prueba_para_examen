8ea36c7ffeaff29922027bd918482efed42cc9f8
mariajesusg <mariajesusg@gmail.com>
2015-06-04 09:26:17 +0200

    me creo dos clases nuevas

---
+++ pp/pp/src/pp/Lista.java
@@ -0,0 +1,13 @@
+package pp;
+
+public class Lista {
+
+	public Lista(int i) {
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+	public void Lista(int i) {
+		// TODO Apéndice de constructor generado automáticamente
+	}
+
+}
---
+++ pp/pp/src/pp/potato.java
@@ -0,0 +1,8 @@
+package pp;
+
+public class potato {
+
+	Lista alumnoDaw=new Lista(2);
+	
+	
+}

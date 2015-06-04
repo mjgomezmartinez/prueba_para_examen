9669c4036cadd6db86685977a50519044af253fc
mariajesusg <mariajesusg@gmail.com>
2015-06-04 09:10:51 +0200

    primera version proyecto

---
+++ pp/pp/src/pp/Ka.java
@@ -0,0 +1,14 @@
+package pp;
+
+public class Ka {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		int i;
+		int j;
+		String cadena;
+		System.out.println("saldrá?");
+	}
+
+}
